856a8d7f678877fec94cf8cf7e4899a5b75c25a0
andrew-pahomov <andrew-pahomov@yandex.ru>
2020-04-22 18:45:29 +0300

    testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2200;
+        int actual = service.remain(amount);
+        int expected = 800;
+        assertEquals(expected, actual);
+    }
+}
