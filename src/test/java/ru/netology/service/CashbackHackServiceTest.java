da59f75c09acfb0eac6ea34f1eed53fea9994d22
andrew-pahomov <andrew-pahomov@yandex.ru>
2020-04-22 19:40:49 +0300

    junit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,26 +1,24 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 2000;
+        int amount = 1000;
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual,expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainWithCashback() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 2200;
+        int amount = 900;
         int actual = service.remain(amount);
-        int expected = 800;
+        int expected = 100;
         assertEquals(actual,expected);
     }
 }
