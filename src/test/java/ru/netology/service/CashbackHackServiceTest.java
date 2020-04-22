9338e2b93afc43706c7331cedf83fe8da2f63611
andrew-pahomov <andrew-pahomov@yandex.ru>
2020-04-22 19:53:26 +0300

    junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,7 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
@@ -15,6 +17,24 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test
     public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testRemainJunit5() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual,expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainWithCashbackJunit5() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
